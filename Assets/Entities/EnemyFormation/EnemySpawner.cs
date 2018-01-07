a719458c12e5bc91e18276f62913a87e6ed569c8
mkkemp94 <mkkemp94@gmail.com>
2018-01-07 16:55:15 -0500

    Add positions to enemy spawner and instantiate enemies on top

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		
+		// For every child in this transform...
+		foreach (Transform child in transform) {
+			
+			// Create an enemy at the origin.
+			GameObject enemy = Instantiate(enemyPrefab, new Vector3(child.position.x, child.position.y, child.position.z), Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

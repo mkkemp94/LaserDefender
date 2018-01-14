e246749fea7b0f12e803a662a75ec33cd0b6b633
mkkemp94 <mkkemp94@gmail.com>
2018-01-14 11:03:02 -0500

    Moving the enemy formation

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -5,20 +5,57 @@ public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
 	
+	public float formationWidth = 10f;
+	public float formationHeight = 5f;
+	
+	public int speed = 1;
+	
+	private bool movingRight = true;
+	
+	private float xmin;
+	private float xmax;
+	
 	// Use this for initialization
 	void Start () {
 		
+		// Get edges of screen
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+		Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+		xmin = leftEdge.x;
+		xmax = rightEdge.x;
+		
+		
 		// For every child in this transform...
-		foreach (Transform child in transform) {
+		foreach (Transform child in this.transform) {
 			
 			// Create an enemy at the origin.
-			GameObject enemy = Instantiate(enemyPrefab, new Vector3(child.position.x, child.position.y, child.position.z), Quaternion.identity) as GameObject;
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
 		}
+	}
+	
+	public void OnDrawGizmos() {
+	
+		// Draw cube around formation
+		Gizmos.DrawWireCube(transform.position, new Vector3(formationWidth, formationHeight));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		// Move enemy formation.
+		if (movingRight) {
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
+		} else {
+			this.transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+		// Switch direction if at edge
+		float rightEdgeOfFormation = this.transform.position.x + (0.6f * formationWidth);
+		float leftEdgeOfFormation = this.transform.position.x - (0.6f * formationWidth);
+		if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
+			movingRight = !movingRight;
+		}
 	}
 }

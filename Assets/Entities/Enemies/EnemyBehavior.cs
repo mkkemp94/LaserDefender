928e977d177b25d1bcc1e04d56d1ead106e54947
mkkemp94 <mkkemp94@gmail.com>
2018-01-14 18:15:30 -0500

    Destroy enemies

---
+++ Assets/Entities/Enemies/EnemyBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+	public float health = 150;
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		missile.Hit();
+		if (missile) {
+			health -= missile.GetDamage();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}

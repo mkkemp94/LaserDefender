35cbf0ccd3d43c655b50f85a97445cb92669adb7
mkkemp94 <mkkemp94@gmail.com>
2018-02-11 15:58:21 -0500

    Idk + git ignore

---
+++ Assets/Entities/Enemies/EnemyBehavior.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+	This script is attached to each enemy.
+*/
+public class EnemyBehavior : MonoBehaviour {
+
+	public float health = 150;
+	public GameObject projectile;
+	public float projectileSpeed = 10;
+	public float shotsPerSecond = 0.5f;
+	public int scoreValue = 150;
+	private ScoreKeeper scorekeeper;
+	
+	public AudioClip fireSound;
+	public AudioClip destroySound;
+	
+	void Start() {
+		scorekeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+	}
+	
+	void Update() {
+		
+		// Fire randomly, based off frame rate
+		float probability = Time.deltaTime * shotsPerSecond;
+		if (Random.value < probability) {
+			Fire();
+		}
+	}
+	
+	// Shoot a missile
+	void Fire() {
+		Vector3 startPosition = transform.position + new Vector3(0, -1f, 0);
+		GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+		missile.rigidbody2D.velocity = new Vector3(0, -projectileSpeed, 0);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+	
+	// When something enters this trigger space
+	void OnTriggerEnter2D(Collider2D collider) {
+	
+		// Get the projectile object while is colliding with this enemy
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		
+		// If the projectile exists
+		if (missile) {
+		
+			// Destroy the missile
+			missile.Destroy();
+			
+			// Reduce the health of this enemy by the missile's damage
+			health -= missile.GetDamage();
+			if (health <= 0) {
+				Die();
+			}
+		}
+	}
+	
+	void Die() {
+		AudioSource.PlayClipAtPoint(destroySound, transform.position);
+		Destroy(gameObject);
+		scorekeeper.Score(scoreValue);
+	}
+	
+	
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	private Text scoreText;
+	public int score = 0;
+	
+	void Start() {
+		scoreText = GetComponent<Text>();
+		Reset();
+	}
+	
+	public void Score (int points) {
+		Debug.Log("Scored!");
+		score += points;
+		scoreText.text = score.ToString();
+	}
+	
+	public void Reset () {
+		score = 0;
+		scoreText.text = score.ToString();
+	}
+}
